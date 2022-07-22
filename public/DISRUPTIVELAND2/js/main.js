e607eb5d6b52482c08ad82037ea4ec82c9615a26
Jorge Perez <jorge.jlpm987@gmail.com>
2022-07-21 21:10:54 -0400

    DASHBOARD.

--- public/DISRUPTIVELAND2/js/main.js
+++ public/DISRUPTIVELAND2/js/main.js
@@ -11,17 +11,9 @@ window.addEventListener('load', function(){
         responsive: [
             {
               // screens greater than >= 775px
-              breakpoint: 992,
+              breakpoint: 650,
               settings: {
                 // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: 3,
-                slidesToScroll: 1
-                // itemWidth: 150,
-              }
-            },{
-              // screens greater than >= 1024px
-              breakpoint: 1024,
-              settings: {
                 slidesToShow: 3,
                 slidesToScroll: 1
                 // itemWidth: 150,
